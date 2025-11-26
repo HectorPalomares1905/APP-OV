4ec053cdbb95374c95dd856e4f5a01b4209ab5ba
Héctor Miguel Palomares Maldonado <128626073+HectorPalomares1905@users.noreply.github.com>
2025-11-25 22:31:51 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// ICATHI 4.0 - Test Vocacional
+// Los enlaces a los formularios están directamente en el HTML
+// No se requiere JavaScript adicional para la funcionalidad básica
+
+// Si en el futuro quieres agregar funcionalidades extra,
+// puedes añadir el código aquí
+
+console.log('ICATHI 4.0 - Aplicación cargada correctamente');
